d05bf98c1fbec48a64dce77bedcff1399619eaf4
Mariia Kopniak <mashakop123@gmail.com>
2025-06-10 00:32:55 +0300

    fixed package issue

--- src/application/Register.java
+++ src/application/Register.java
@@ -6,7 +6,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
-import java.util.Scanner;
 
 public class Register {
     public static void registerUser(String username, String email, String password) {
